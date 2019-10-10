b7608d13182e3868b7a83810661c23ebf4101e15
Bret Walker <bw@bretw.com>
2019-10-09 20:26:24 -0400

    ACLU-specific

--- app.py
+++ app.py
@@ -10,99 +10,113 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 load_dotenv()
 
-SPREADSHEET_NAME = os.getenv("SPREADSHEET_NAME", "Prefiled Bills")
+SPREADSHEET_URL = os.getenv("SPREADSHEET_URL")
 CLIENT_SECRET = json.loads(os.getenv("CLIENT_SECRET"))
 PREFILED_BILLS_PAGE = os.getenv(
     "PREFILED_BILLS_PAGE", "https://apps.legislature.ky.gov/record/20rs/prefiled/prefiled_bills.html"
 )
+PAGES = {
+    'Prefiled Bills': PREFILED_BILLS_PAGE,
+}
 BILL_REQUEST_URL_RE = r"(BR\d+\.html)"
-SUMMARY_SHEET_NAME = "Summary"
-SUMMARY_SHEET_HEADERS = ["Bill Request", "KEJC Category & Summarizer", "KEJC Summary", "Status"]
 
-BILL_ROWS = [
-    "Link to Bill Text",
-    "KEJC Category & Summarizer",
-    "KEJC Summary",
-    "Title",
-    "Sponsors",
-    "Summary of Original Version",
-    "Index Headings",
-]
-
+SHEET_HEADERS = ["Bill Number", "Bill Title", "Bill Sponsors", "Last Action"]
+MAX_COLUMN = chr(97 + len(SHEET_HEADERS)-1).upper()
 
 def run():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     creds = ServiceAccountCredentials.from_json_keyfile_dict(CLIENT_SECRET, scope)
     client = gspread.authorize(creds)
-    spreadsheet = client.open(SPREADSHEET_NAME)
+    spreadsheet = client.open_by_url(SPREADSHEET_URL)
     worksheet_titles = [s.title for s in spreadsheet.worksheets()]
 
-    _create_summary_sheet(spreadsheet, worksheet_titles)
-
-    page = urllib.request.urlopen(PREFILED_BILLS_PAGE)
-    soup = BeautifulSoup(page, "html.parser")
-
-    bill_request_urls = re.findall(BILL_REQUEST_URL_RE, str(soup))
-    bill_request_urls.sort()
-
-    for url in bill_request_urls:
-        bill_number = url.replace(".html", "")
-        if bill_number in worksheet_titles:
-            print(f"Skipping bill {bill_number}, which already exists")
-            continue
-
-        values = [None for i in range(len(BILL_ROWS))]
-
-        page = urllib.request.urlopen(PREFILED_BILLS_PAGE.replace("prefiled_bills.html", url))
+    for sheet_name, page_url in PAGES.items():
+        rows = []
+        page = urllib.request.urlopen(page_url)
         soup = BeautifulSoup(page, "html.parser")
-        table_body = soup.find("tbody")
-        rows = table_body.find_all("tr")
-        for row in rows:
-            header = row.find_all("th")[0].text.strip()
-            if header in BILL_ROWS:
-                values[BILL_ROWS.index(header)] = row.find_all("td")[0].text.strip()
-        _add_bill(bill_number, values, spreadsheet)
+
+        _create_sheet(spreadsheet, sheet_name, worksheet_titles)
+
+        bill_request_urls = re.findall(BILL_REQUEST_URL_RE, str(soup))
+        bill_request_urls.sort(key=lambda u: int(re.sub(".html", "", re.sub("BR", "", u))))
+
+        added_bills = spreadsheet.worksheet(sheet_name).col_values(1)
+        start_at_row = len(added_bills) + 1
+
+        for url in bill_request_urls:
+            bill_number = url.replace(".html", "")
+            if bill_number in added_bills:
+                print(f"Skipping bill {bill_number}, which already exists")
+                continue
+
+            values = [None for i in range(len(SHEET_HEADERS))]
+
+            page = urllib.request.urlopen(PREFILED_BILLS_PAGE.replace("prefiled_bills.html", url))
+            soup = BeautifulSoup(page, "html.parser")
+
+            tables = soup.find_all("div", {"class": "bill-table"})
+            if len(tables) > 0:
+                bill_detail_rows = tables[0].find("tbody").find_all("tr")
+                for row in bill_detail_rows:
+                    header = row.find_all("th")[0].text.strip()
+                    values[0] = bill_number
+                    if f"Bill {header}" in SHEET_HEADERS:
+                        values[SHEET_HEADERS.index(f"Bill {header}")] = re.sub(r"\s+", " ", row.find_all("td")[0].text.strip())
+                if len(tables) > 1:
+                    bill_action_rows = tables[1].find("tbody").find_all("tr")
+                    if bill_action_rows:
+                        last_row = bill_action_rows[-1]
+                        values[SHEET_HEADERS.index("Last Action")] = last_row.find_all("td")[0].text.strip()
+                    else:
+                        print(f"Didn't find any actions for {bill_number}")
+                else:
+                    print(f"Skipping actions for {bill_number}")
+                print(f"Adding {values}")
+                rows.append(values)
+            else:
+                print(f"Could not find bill table for {bill_number}")
+
+        _add_bills(rows, start_at_row, sheet_name, spreadsheet)
 
 
-def _add_bill(bill_number, values, spreadsheet):
-    print(f"Adding bill number {bill_number}")
-    summary_sheet = spreadsheet.worksheet("Summary")
-    bill_sheet = spreadsheet.add_worksheet(bill_number, 5, 2)
+def _add_bills(rows, start_at_row, sheet_name, spreadsheet):
+    if not rows:
+        return
+    max_row = len(rows) + start_at_row - 1
+    num_columns = len(SHEET_HEADERS)
+    worksheet = spreadsheet.worksheet(sheet_name)
+    cell_list = worksheet.range(f"B{start_at_row}:{MAX_COLUMN}{max_row}")
 
-    cell_list = bill_sheet.range("A1:A7")
-    i = 0
+    row_num = 0
+    column_number = 1
     for cell in cell_list:
-        cell.value = BILL_ROWS[i]
-        i += 1
-    bill_sheet.update_cells(cell_list)
+        if column_number == 1:
+            worksheet.update_cell(
+                cell.row, 1,
+                f'=HYPERLINK("https://apps.legislature.ky.gov/recorddocuments/bill/20RS/{rows[row_num][0]}/orig_bill.pdf", "{rows[row_num][0]}")',
+            )
+        cell.value = rows[row_num][column_number]
+        if column_number == (num_columns - 1):
+            row_num += 1
+            column_number = 1
+        else:
+            column_number += 1
 
-    cell_list = bill_sheet.range("B1:B7")
-    i = 0
-    for cell in cell_list:
-        cell.value = values[i]
-        i += 1
-    bill_sheet.update_cells(cell_list)
-    bill_sheet.update_acell(
-        "B1",
-        f'=HYPERLINK("https://apps.legislature.ky.gov/recorddocuments/bill/20RS/{bill_number}/orig_bill.pdf", "Original Bill PDF")',
-    )
-
-    ss_last_row = len(summary_sheet.col_values(1)) + 1
-    summary_sheet.update_cell(ss_last_row, 1, bill_number)
+    worksheet.update_cells(cell_list)
 
 
-def _create_summary_sheet(spreadsheet, worksheet_titles):
-    if SUMMARY_SHEET_NAME not in worksheet_titles:
-        spreadsheet.add_worksheet(SUMMARY_SHEET_NAME, 500, 4)
+def _create_sheet(spreadsheet, sheet_name, worksheet_titles):
+    if sheet_name not in worksheet_titles:
+        spreadsheet.add_worksheet(sheet_name, 500, 8)
 
-    summary_sheet = spreadsheet.worksheet(SUMMARY_SHEET_NAME)
+    summary_sheet = spreadsheet.worksheet(sheet_name)
 
     if not summary_sheet.row_values(1):
-        cell_list = summary_sheet.range("A1:D1")
+        cell_list = summary_sheet.range(f"A1:{MAX_COLUMN}1")
 
         i = 0
         for cell in cell_list:
-            cell.value = SUMMARY_SHEET_HEADERS[i]
+            cell.value = SHEET_HEADERS[i]
             i += 1
 
         summary_sheet.update_cells(cell_list)
